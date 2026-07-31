27e28d7c58436744d35abbc9849a20240be05b47
h0p3so <juan.dpmz05@gmail.com>
2026-07-31 11:08:22 -0500

    parte 1 de la parte dos hecha

--- src/main/java/edu/eci/arsw/threads/CountThread.java
+++ src/main/java/edu/eci/arsw/threads/CountThread.java
@@ -1,4 +1,16 @@
 package edu.eci.arsw.threads;
 
-public class CountThread {    
+public class CountThread implements Runnable {
+	private int upto;
+
+	public CountThread (final int upto) {
+		this.upto = upto;
+	}
+
+	@Override
+	public void run () {
+		for (int i = 0; i < this.upto; i++) {
+			System.out.println("counting: " + i);
+		}
+	}
 }
--- src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
+++ src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
@@ -1,7 +1,12 @@
 package edu.eci.arsw.threads;
 
+import java.lang.Thread;
+
 public class CountThreadsMain {
-    public static void main(String a[]){
-		System.out.println("Hello world");
-    }
+	public static void main(String args[]) {
+		CountThread t = new CountThread(10);
+		Thread thread = new Thread(t);
+
+		thread.start();
+	}
 }
