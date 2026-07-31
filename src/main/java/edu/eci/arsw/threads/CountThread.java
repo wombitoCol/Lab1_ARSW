a2a4b5a71c66632bac551499556c4c5aaa42c60f
wombitoCol <julio.mayorquin@mail.escuelaing.edu.co>
2026-07-31 12:43:16 -0500

    part two

--- src/main/java/edu/eci/arsw/threads/CountThread.java
+++ src/main/java/edu/eci/arsw/threads/CountThread.java
@@ -1,16 +1,24 @@
 package edu.eci.arsw.threads;
 
 public class CountThread implements Runnable {
-	private int upto;
+	private int start;
+	private int amount;
+	private int finals;
 
-	public CountThread (final int upto) {
-		this.upto = upto;
+	public CountThread (int start, int amount) {
+		this.start = start;
+		this.amount = amount;
 	}
 
 	@Override
 	public void run () {
-		for (int i = 0; i < this.upto; i++) {
+		for (int i = this.start; i < this.amount; i++) {
 			System.out.println("counting: " + i);
+			finals = i;
 		}
+	}
+
+	public int getFinal () {
+		return this.finals;
 	}
 }
