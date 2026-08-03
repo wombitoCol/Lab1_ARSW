b9b7715f76edc30abe88958cc37e6f965f0440c8
h0p3so <juan.dpmz05@gmail.com>
2026-08-03 18:38:49 -0500

    parte 1.2

--- src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
+++ src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
@@ -7,9 +7,6 @@ public class CountThreadsMain {
     private static int last3;
 
 	public static void main(String args[]) {
-
-        
-
         CountThread t = new CountThread(0, 99);
         CountThread l = new CountThread(t.getFinal(), 199);
         CountThread s = new CountThread(l.getFinal(), 299);
@@ -20,5 +17,11 @@ public class CountThreadsMain {
 		thread.start();
         thread2.start();
         thread3.start();
+
+        /* Parte solicitada por el profesor */
+        int start = 0, upto = 1;
+        CountThread x = new CountThread(start, upto);
+        Thread xx = new Thread(x);
+        xx.start();
 	}
 }
